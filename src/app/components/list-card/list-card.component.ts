c7ae136201453b51bcd55f1600a1dd22e0cc0a2a
SaySubr <165163931+SaySubr@users.noreply.github.com>
2025-08-27 21:42:02 +0700

    add material table (test)

--- src/app/components/list-card/list-card.component.ts
+++ src/app/components/list-card/list-card.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 import { HttpServiceService } from '../../http-service.service';
 import { Router } from '@angular/router';
+import {MatTableModule} from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+
+export interface PeriodicElement {
+  type: string;
+  creditcard: number;
+  Validity: number;
+  balance: string;
+}
+
 
 @Component({
   selector: 'app-list-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatButtonModule,MatTableModule],
   templateUrl: './list-card.component.html',
   styleUrl: './list-card.component.scss'
 })
 export class ListCardComponent {
-creditCards: any;
-
+  creditCards: any;
   constructor(private httpService: HttpServiceService, private router: Router){
-
   }
 
  GetDataCard() {
@@ -24,4 +31,7 @@ creditCards: any;
     });
   }
 
+
+   displayedColumns: string[] = ['type', 'creditcard', 'Validity', 'balance'];
+  clickedRows = new Set<PeriodicElement>();
 }
