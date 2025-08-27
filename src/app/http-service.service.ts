1449363bdccdea652fb71f8979ca81ad48dca42d
SaySubr <165163931+SaySubr@users.noreply.github.com>
2025-08-27 20:22:48 +0700

    Update Service

--- src/app/http-service.service.ts
+++ src/app/http-service.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 
 @Injectable({
@@ -13,6 +13,20 @@ export class HttpServiceService {
    private apiUrl = 'https://fakerapi.it/api/v2/creditCards?_quantity=10';
 
    getCreditCards(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+    return this.http.get<any>(this.apiUrl).pipe(
+      map(response => {
+       
+        response.data = response.data.map((card: any) => ({
+          ...card,
+          balance: this.getRandomBalance()
+        }));
+        return response;
+      })
+    );
+  }
+
+
+  private getRandomBalance(): number {
+    return +(Math.random() * (10000 - 100) + 100).toFixed(2);
   }
 }
